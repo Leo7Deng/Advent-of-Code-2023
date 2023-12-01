422bd9861f153110893a4b5a05963a4e9c173ed6
Leo Deng <leo7deng@gmail.com>
2023-12-01 18:22:47 -0500

    day 1 part 1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -4,8 +4,33 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(input: &str) -> String {
-    "142".to_string()
+fn part1(input: &str) -> u32 {
+    let mut sum :u32 = 0;
+    for line in input.lines() {
+        let mut num1 :u32 = 0;
+        let mut num2 :u32 = 0;
+        let mut iterate :u32 = 0;
+        for (i, c) in line.chars().enumerate() {
+            if c.is_numeric() {
+                if iterate == 0 {
+                    num1 = c.to_digit(10).unwrap();
+                    iterate = 1;
+                }
+                else if iterate >= 1 {
+                    num2 = c.to_digit(10).unwrap();
+                    iterate = 2;
+                }
+            }
+            if i == line.len()-1 && iterate == 1 {
+                num2 = num1;
+            }
+        }
+        dbg!(format!("{}{}", num1, num2).to_string());
+        sum += format!("{}{}", num1, num2).to_string().parse::<u32>().unwrap();
+        
+
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -18,6 +43,6 @@ mod tests {
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet");
-        assert_eq!(result, "142".to_string());
+        assert_eq!(result, 142);
     }
 }
