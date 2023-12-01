6d2ae298bab89e83506e78d44828d1fa9b2ac507
Leo Deng <leo7deng@gmail.com>
2023-12-01 17:20:23 -0500

    setup

---
+++ day-01/src/bin/part1.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    "142".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1("1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet");
+        assert_eq!(result, "142".to_string());
+    }
+}
